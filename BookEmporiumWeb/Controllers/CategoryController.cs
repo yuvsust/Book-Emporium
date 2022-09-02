0afad90ba4e8bd50f408fc3f1f4eb8ea97d83d4f
yuvsust <tanvirhossainyuvraz@gmail.com>
2022-09-02 16:59:34 +0600

    Added bootswatchTheme, created category controller.

---
+++ BookEmporiumWeb/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BookEmporium.DataAccess.Data;
+using BookEmporium.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookEmporiumWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _db.Categories;
+            return View(categoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
