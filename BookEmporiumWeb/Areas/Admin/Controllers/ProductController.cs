3fd35b5d66ba316c9766c89f9642a46105a4ff5d
yuvsust <tanvirhossainyuvraz@gmail.com>
2022-09-04 20:50:50 +0600

    Added product module

---
+++ BookEmporiumWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using BookEmporium.DataAccess.Repository.IRepository;
+using BookEmporium.Models;
+using BookEmporium.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookEmporiumWeb.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWOrk;
+        public ProductController(IUnitOfWork unitOfWOrk)
+        {
+            _unitOfWOrk = unitOfWOrk;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _unitOfWOrk.Product.GetAll();
+            return View(objProductList);
+        }
+
+        // GET
+        public IActionResult Upsert(int? id)
+        {
+            ProductViewModel productViewModel = new ProductViewModel()
+            {
+                Product = new Product(),
+                CategoryList = _unitOfWOrk.Category.GetAll().Select(u => new SelectListItem()
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWOrk.CoverType.GetAll().Select(u => new SelectListItem()
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                })
+            };
+            if (id == null || id == 0)
+            {
+                //create
+                return View(productViewModel);
+            }
+            else
+            {
+                //update
+                return View();
+            }
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductViewModel productViewModel, IFormFile? file)
+        {
+            if(ModelState.IsValid)
+            {
+                
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookEmporiumWeb/wwwroot/js/product-scripts.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    
+});
+
+function validateImageInput() {
+    if ($('#imageUrl').val() == "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please upload a valid image!',
+        })
+        return false;
+    } else {
+        return true;
+    }
+}
